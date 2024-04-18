cd5fa0a1e93638213f3836c126d6d5ec40155dfc
arcilac <arcilacamila08@gmail.com>
2024-04-17 20:49:56 -0500

    global state

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -1,16 +1,40 @@
 import React from "react"
-import { useAppSelector } from "../../app/hooks"
-import { getTotalPrice } from "./cartSlice"
+import classNames from "classNames"
+import { useAppSelector, useAppDispatch } from "../../app/hooks"
+import { checkoutCart, getTotalPrice, removeFromCart, updateQuantity } from "./cartSlice"
 import styles from "./Cart.module.css"
 
 export default function Cart() {
+  const dispatch = useAppDispatch
  const products = useAppSelector((state) => state.products.products)
  const items = useAppSelector((state) => state.cart.items)
  const totalPrice = useAppSelector(getTotalPrice)
+ const checkoutState = useAppSelector(state => state.cart.checkoutState)
+ const errorMessage = useAppSelector(state => state.cart.errorMessage)
+
+ function onQuantityChanged(
+  e: React.FocusEvent<HTMLInputElement>,
+  id: string
+  ) {
+  const quantity = Number(e.target.value) || 0
+  dispatch(updateQuantity({ id, quantity }))
+ }
+
+ function onCheckout(e: React.FormEvent<HTMLFormElement>) {
+  e.preventDefault()
+  dispatch(checkoutCart(items))
+ }
+
+ const tableClasses = classNames({
+  [styles.table]: true,
+  [styles.checkoutError]: checkoutState === "ERROR",
+  [styles.checkoutLoading]: checkoutState === "LOADING",
+ })
+
  return (
   <main className="page">
     <h1>Shopping Cart</h1>
-    <table className={styles.table}>
+    <table className={tableClasses}>
       <thead>
         <tr>
           <th>Product</th>
@@ -24,11 +48,13 @@ export default function Cart() {
           <tr>
           <td>{products[id].name}</td>
           <td>
-            <input type="text" className={styles.input} defaultValue={quantity} />
+            <input type="text" className={styles.input} defaultValue={quantity} onBlur={(e) => onQuantityChanged(e, id)} />
           </td>
           <td>${products[id].price}</td>
           <td>
-            <button aria-label={`Remove ${products[id].name} from Shopping Cart`}>
+            <button aria-label={`Remove ${products[id].name} from Shopping Cart`}
+            onClick={() => dispatch(removeFromCart(id))}
+            >
               X
             </button>
           </td>
@@ -45,7 +71,9 @@ export default function Cart() {
         </tr>
       </tfoot>
     </table>
-    <form>
+    <form onSubmit={onCheckout}>
+      {checkoutState === "ERROR" && errorMessage ? (<p className={styles.errorBox}>{errorMessage}</p>
+    ) : null}
       <button className={styles.button} type="submit">
         Checkout
       </button>
--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,13 +1,27 @@
-import { createSlice, createSelector, PayloadAction } from "@reduxjs/toolkit"
-import { RootState } from "../../app/store"
+import { createSlice, createAsyncThunk, createSelector, PayloadAction } from "@reduxjs/toolkit"
+import { checkout, CartItems } from "../../app/api"
+import type { RootState } from "../../app/store"
 
+
+type CheckoutState = "LOADING" | "READY" | "ERROR"
 export interface CartState {
     items: { [id: string]: number }
+    checkoutState: CheckoutState
+    errorMessage: string
 }
 
 const initialState: CartState = {
-    items: {}
+    items: {},
+    checkoutState: "READY",
+    errorMessage: ""
 }
+
+export const checkoutCart = createAsyncThunk<{success: boolean}>("cart/checkout", async ( , thunkAPI) => {
+    const state = thunkAPI.getState()
+    const items = state.cart.items
+    const response = await checkout(items)
+    return response
+})
 
 const cartSlice = createSlice ({
     name: "cart",
@@ -20,11 +34,42 @@ const cartSlice = createSlice ({
             } else {
                 state.items[id] = 1
             }
-        }
+        },
+        removeFromCart(state, action: PayloadAction<string>) {
+            delete state.items[action.payload]
+        },
+        updateQuantity(
+            state, 
+            action: PayloadAction<{ id: string, quantity: number }>
+        ) {
+            const { id, quantity } = action.payload
+            state.items[id] = quantity
+        },
+    },
+    extraReducers: function (builder) {
+        builder.addCase(checkoutCart.pending, (state) => {
+            state.checkoutState = "LOADING"
+        })
+        builder.addCase(checkoutCart.fulfilled, (state, action: PayloadAction<{ success: boolean }>) => {
+            const { success } = action.payload
+            if (success) {
+                state.checkoutState = "READY"
+                state.items = {}
+            } else {
+                state.checkoutState = "ERROR"
+            }
+            
+        })
+        builder.addCase(checkoutCart.rejected, (state, action) => {
+            state.checkoutState = "ERROR"
+            state.errorMessage = action.error.message || ""
+        })
     }
 })
 
-export const { addToCart } = cartSlice.actions
+
+
+export const { addToCart, removeFromCart, updateQuantity } = cartSlice.actions
 export default cartSlice.reducer
 
 export function getNumItems(state: RootState) {
